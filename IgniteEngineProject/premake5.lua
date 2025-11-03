2a181f83c21afb3faa7f60afa0fb537514cda826
Hidd3n47 <122316898+Hidd3n-47@users.noreply.github.com>
2025-11-03 18:41:18 +0000

    Logger added to be used for engine

---
+++ IgniteEngineProject/premake5.lua
@@ -0,0 +1,40 @@
+-- ---------------------------- IgniteEngine Project.
+project "IgniteEngine"
+    location "%{prj.name}"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "Off"
+    cppdialect "C++latest"
+
+    targetdir(outputPath .. "%{prj.name}")
+    objdir(outputIntPath .. "%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/**.h",
+        "%{prj.name}/**.cpp",
+    }
+
+    includedirs
+    {
+    }
+
+    libdirs
+    {
+    }
+
+    links
+    {
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Dev"
+        runtime "Debug"
+        defines "DEV_CONFIGURATION"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
