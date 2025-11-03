3c773d1569ea2a99cdaa169ec77ae8b8b6d0806b
Hidd3n47 <122316898+Hidd3n-47@users.noreply.github.com>
2025-11-03 18:29:36 +0000

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,16 @@
+workspace "Ignite"
+architecture "x64"
+
+configurations
+{
+    "Dev",
+    "Release"
+}
+
+outputName    = "%{cfg.buildcfg}"
+outputPath    = "$(SolutionDir)Scratch/Bin/" .. outputName .. "/"
+outputIntPath = "$(SolutionDir)Scratch/Int/" .. outputName .. "/"
+
+include "IgniteEngineProject/"
+
+include "LogProject/"
